ab2c130985434254adcb842a559662dd7e148b6c
iamaswinth <iamaswinth@gmail.com>
2024-12-03 08:03:30 +0530

    feat:news

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ const navLink = document.querySelectorAll(`.nav__link`);
 
 const linkAction = () =>{
     const navMenu = document.getElementById(`nav-menu`)
-    navMenu.classList.remove('show-menu')
+    navMenu.classList.remove('show-menu')   
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
